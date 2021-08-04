073484fc7fa651d62e70b9c82b08b1d2d7cb2433
juansv2k2 <juansv2k2@hotmail.com>
2021-08-04 19:09:12 +0200

    app.js y views

--- app.js
+++ app.js
@@ -12,5 +12,21 @@ app.listen(3000, () => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+    res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/loginhtml"));
+});
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/productCart.html"));
+});
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/productDetail.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"));
 });
