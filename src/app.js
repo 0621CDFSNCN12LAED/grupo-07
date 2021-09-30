98f234612d4d749e0f214e67ea70dfee8ed10d6e
yasayup <jasayup342@gmail.com>
2021-09-30 17:03:50 -0300

    comenzando express-validator

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.use(logger("dev"));
 app.use(methodOverride("_method"));
 app.use(session({ secret: "Sh! Esto es un secreto" }));
 app.use(cookieParser());
+//uso de metodo para capturar info de forms
+app.use(express.urlencoded({ extended: false }));
 
 //Template engine
 app.set("view engine", "ejs");
@@ -25,9 +27,11 @@ app.set("views", path.join(__dirname, "/views"));
 
 const rutasMain = require("./routes/mainRoute");
 const rutasProducts = require("./routes/productsRoute");
+const rutasUsers = require("./routes/usersRoute");
 
 app.use("/", rutasMain);
 app.use("/products", rutasProducts);
+app.use("/user", rutasUsers);
 
 //Error
 // app.use((req, res, next) => next(createError(404))); <= ARREGLAR
---
+++ src/routes/usersRoute.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const {body} = require("express-validator")
+
+const userController = require("../controllers/userControllers");
+
+const validations = [
+    body("email").isEmail().withMessage("Debe insertar un mail válido"),
+    body("password").notEmpty().withMessage("Debe insertar una contraseña válida para el registro"),
+]
+
+
+//form de registro
+router.get("/register", userController.register);
+//procesar el registro
+router.post("/register", validations, userController.processRegister);
+
+
+//form de login
+router.get("/login", userController.login);
+
+//Perfil del usuario
+router.get("/profile/:userId", userController.profile);
+
+module.exports = router;
