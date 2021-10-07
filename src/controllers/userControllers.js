af8ccd327952ce4fcbf18b6f096f594f26773b39
juansv2k2 <juansv2k2@hotmail.com>
2021-10-08 01:23:52 +0200

    no-roto

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const bcrypt = require("bcryptjs");
 
 const { validationResult } = require("express-validator");
 
@@ -11,28 +12,28 @@ const controller = {
         return res.render("register");
     },
     processRegister: (req, res) => {
-       let errors = validationResult(req);
-       if(errors.isEmpty()){
-        const lastUser = users[users.length - 1];
-        const biggestUserId = users.length > 0 ? lastUser.id : 1;
-        const user = {
-            id: biggestUserId + 1,
-            name: req.body.name,
-            email: req.body.email,
-            birthdate: req.body.birthdate,
-            password: req.body.password,
-            //image: image ? image.filename : "default-image.png",
-            delete: false,
-        };
-        users.push(user);
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+            const lastUser = users[users.length - 1];
+            const biggestUserId = users.length > 0 ? lastUser.id : 1;
+            const user = {
+                id: biggestUserId + 1,
+                name: req.body.name,
+                email: req.body.email,
+                birthdate: req.body.birthdate,
+                password: bcrypt.hashSync(req.body.password, 10),
 
-        const jsonString = JSON.stringify(users, null, 4);
-        fs.writeFileSync(usersFilePath, jsonString);
-        
-       }else{
-           res.render("register", {errors: errors.array(), old: req.body})
-       }
-       return res.send("hola estas en tu perfil");
+                //image: image ? image.filename : "default-image.png",
+                delete: false,
+            };
+            users.push(user);
+
+            const jsonString = JSON.stringify(users, null, 4);
+            fs.writeFileSync(usersFilePath, jsonString);
+        } else {
+            res.render("register", { errors: errors.array(), old: req.body });
+        }
+        return res.send("hola estas en tu perfil");
     },
     login: (req, res) => {
         return res.render("login");
@@ -40,6 +41,6 @@ const controller = {
     profile: (req, res) => {
         return res.render("userProfile");
     },
-}
+};
 
 module.exports = controller;
