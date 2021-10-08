68283e0be1b1d0890ddfa0ed3275b6d33596b304
Bárbara Rivas <barbararivas@MacBook-Air-de-Barbara.local>
2021-10-08 12:55:08 +0200

    más modificaciones css en index

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const path = require("path");
+
+const productService = require("../services/productService");
 const bcrypt = require("bcryptjs");
 
 const { validationResult } = require("express-validator");
@@ -8,39 +10,39 @@ const usersFilePath = path.join(__dirname, "../dataBase/usersDataBase.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
 
 const controller = {
-    register: (req, res) => {
-        return res.render("register");
-    },
-    processRegister: (req, res) => {
-        let errors = validationResult(req);
-        if (errors.isEmpty()) {
-            const lastUser = users[users.length - 1];
-            const biggestUserId = users.length > 0 ? lastUser.id : 1;
-            const user = {
-                id: biggestUserId + 1,
-                name: req.body.name,
-                email: req.body.email,
-                birthdate: req.body.birthdate,
-                password: bcrypt.hashSync(req.body.password, 10),
+  register: (req, res) => {
+    return res.render("register");
+  },
+  processRegister: (req, res) => {
+    let errors = validationResult(req);
+    if (errors.isEmpty()) {
+      const lastUser = users[users.length - 1];
+      const biggestUserId = users.length > 0 ? lastUser.id : 1;
+      const user = {
+        id: biggestUserId + 1,
+        name: req.body.name,
+        email: req.body.email,
+        birthdate: req.body.birthdate,
+        password: bcrypt.hashSync(req.body.password, 10),
 
-                //image: image ? image.filename : "default-image.png",
-                delete: false,
-            };
-            users.push(user);
+        //image: image ? image.filename : "default-image.png",
+        delete: false,
+      };
+      users.push(user);
 
-            const jsonString = JSON.stringify(users, null, 4);
-            fs.writeFileSync(usersFilePath, jsonString);
-        } else {
-            res.render("register", { errors: errors.array(), old: req.body });
-        }
-        return res.send("hola estas en tu perfil");
-    },
-    login: (req, res) => {
-        return res.render("login");
-    },
-    profile: (req, res) => {
-        return res.render("userProfile");
-    },
+      const jsonString = JSON.stringify(users, null, 4);
+      fs.writeFileSync(usersFilePath, jsonString);
+    } else {
+      res.render("register", { errors: errors.array(), old: req.body });
+    }
+    return res.send("hola estas en tu perfil");
+  },
+  login: (req, res) => {
+    return res.render("login");
+  },
+  profile: (req, res) => {
+    return res.render("userProfile");
+  },
 };
 
 module.exports = controller;
---
+++ src/services/userService.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const fs = require("fs");
+
+const usersFilePath = path.join(__dirname, "../dataBase/usersDataBase.json");
+const products = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
+
+const productService = {
+  findOneById(id) {
+    const user = users.find((user) => {
+      return user.id == id;
+    });
+    return user;
+  },
+
+  createOne(payload, image) {
+    const lastUser = users[users.length - 1];
+    const biggestUserId = users.length > 0 ? lastUser.id : 1;
+    const user = {
+      ...payload,
+      id: biggestUserId + 1,
+      name: req.body.name,
+      email: req.body.email,
+      birthdate: req.body.birthdate,
+      password: bcrypt.hashSync(req.body.password, 10),
+      image: image ? image.filename : "default-image.png",
+      delete: false,
+    };
+    users.push(user);
+    this.save();
+  },
+
+  editOne(id, payload, image) {
+    const user = this.findOneById(id);
+    user.name = payload.name;
+    user.email = payload.email;
+    user.birthdate = payload.birthdate;
+    user.password = payload.password;
+    user.image = image ? image.filename : user.image;
+    this.save();
+  },
+  destroyOne(id) {
+    const user = this.findOneById(id);
+    user.deleted = true;
+    this.save();
+  },
+
+  save() {
+    const jsonString = JSON.stringify(users, null, 4);
+    fs.writeFileSync(usersFilePath, jsonString);
+  },
+};
+
+module.exports = userService;
