7d49cf4a30e85bb93b6b712574f05b4ba5f9d00a
Bárbara Rivas <barbararivas@MacBook-Air-de-Barbara.local>
2021-10-14 11:08:11 +0200

    moví user model a services, hice modificaciones en la vista de userProfile

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,97 +1,98 @@
-const bcryptjs = require('bcryptjs');
-const {
-	validationResult
-} = require('express-validator');
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
-const User = require('../models/User');
+const User = require("../services/userService");
 
 const controller = {
-	register: (req, res) => {
-		return res.render('userRegisterForm');
-	},
-	processRegister: (req, res) => {
-		const resultValidation = validationResult(req);
+  register: (req, res) => {
+    return res.render("userRegisterForm");
+  },
+  processRegister: (req, res) => {
+    const resultValidation = validationResult(req);
 
-		if (resultValidation.errors.length > 0) {
-			return res.render('userRegisterForm', {
-				errors: resultValidation.mapped(),
-				oldData: req.body
-			});
-		}
+    if (resultValidation.errors.length > 0) {
+      return res.render("userRegisterForm", {
+        errors: resultValidation.mapped(),
+        oldData: req.body,
+      });
+    }
 
-		let userInDB = User.findByField('email', req.body.email);
+    let userInDB = User.findByField("email", req.body.email);
 
-		if (userInDB) {
-			return res.render('userRegisterForm', {
-				errors: {
-					email: {
-						msg: 'Este email ya está registrado'
-					}
-				},
-				oldData: req.body
-			});
-		}
+    if (userInDB) {
+      return res.render("userRegisterForm", {
+        errors: {
+          email: {
+            msg: "Este email ya está registrado",
+          },
+        },
+        oldData: req.body,
+      });
+    }
 
-		//--> si no tengo errors, creo un usuario (sigo el proceso)
-		//--> por un lado, traigo todo lo que trajo el request del body
-		//--> incluyendo la propiedad "avatar" con el nombre del archivo, que me trae el request
-		//--> y la propiedad password hasheada usando libreria bycript, usando lo que viene en body.
-		let userToCreate = {
-			...req.body,
-			password: bcryptjs.hashSync(req.body.password, 10),
-			avatar: req.file.filename
-		}
+    //--> si no tengo errors, creo un usuario (sigo el proceso)
+    //--> por un lado, traigo todo lo que trajo el request del body
+    //--> incluyendo la propiedad "avatar" con el nombre del archivo, que me trae el request
+    //--> y la propiedad password hasheada usando libreria bycript, usando lo que viene en body.
+    let userToCreate = {
+      ...req.body,
+      password: bcryptjs.hashSync(req.body.password, 10),
+      avatar: req.file.filename,
+    };
 
-		let userCreated = User.create(userToCreate);
+    let userCreated = User.create(userToCreate);
 
-		return res.redirect('/user/login');
-	},
-	login: (req, res) => {
-		return res.render('userLoginForm');
-	},
-	loginProcess: (req, res) => {
-		let userToLogin = User.findByField('email', req.body.email);
-		
-		if(userToLogin) {
-			let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
-			if (isOkThePassword) {
-				delete userToLogin.password;
-				req.session.userLogged = userToLogin;
+    return res.redirect("/user/login");
+  },
+  login: (req, res) => {
+    return res.render("userLoginForm");
+  },
+  loginProcess: (req, res) => {
+    let userToLogin = User.findByField("email", req.body.email);
 
-				if(req.body.remember_user) {
-					res.cookie('userEmail', req.body.email, { maxAge: (1000 * 60) * 60 })
-				}
+    if (userToLogin) {
+      let isOkThePassword = bcryptjs.compareSync(
+        req.body.password,
+        userToLogin.password
+      );
+      if (isOkThePassword) {
+        delete userToLogin.password;
+        req.session.userLogged = userToLogin;
 
-				return res.redirect('/user/userProfile');
-			} 
-			return res.render('userLoginForm', {
-				errors: {
-					email: {
-						msg: 'Las credenciales son inválidas'
-					}
-				}
-			});
-		}
+        if (req.body.remember_user) {
+          res.cookie("userEmail", req.body.email, { maxAge: 1000 * 60 * 60 });
+        }
 
-		return res.render('userLoginForm', {
-			errors: {
-				email: {
-					msg: 'No se encuentra este email en nuestra base de datos'
-				}
-			}
-		});
-	},
-	profile: (req, res) => {
-		return res.render('userProfile', {
-			user: req.session.userLogged
-		});
-	},
+        return res.redirect("/user/userProfile");
+      }
+      return res.render("userLoginForm", {
+        errors: {
+          email: {
+            msg: "Las credenciales son inválidas",
+          },
+        },
+      });
+    }
 
-	logout: (req, res) => {
-		res.clearCookie('userEmail');
-		req.session.destroy();
-		return res.redirect('/');
-	}
-}
+    return res.render("userLoginForm", {
+      errors: {
+        email: {
+          msg: "No se encuentra este email en nuestra base de datos",
+        },
+      },
+    });
+  },
+  profile: (req, res) => {
+    return res.render("userProfile", {
+      user: req.session.userLogged,
+    });
+  },
+
+  logout: (req, res) => {
+    res.clearCookie("userEmail");
+    req.session.destroy();
+    return res.redirect("/");
+  },
+};
 
 module.exports = controller;
