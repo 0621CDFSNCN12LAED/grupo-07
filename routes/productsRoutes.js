30183affe40cf30559e30651154eb3567e0c3931
juansv2k2 <juansv2k2@hotmail.com>
2021-10-16 18:22:51 +0200

    fixed create and edit

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/routes/productsRoute.js
 //Require
 
 const express = require("express");
@@ -7,14 +6,14 @@ const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../../public/img/product-images"),
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
+    destination: path.join(__dirname, "../public/images/product-images"),
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
 });
 
 const uploader = multer({
-  storage,
+    storage,
 });
 
 const productsControllers = require("../controllers/productsControllers");
@@ -23,49 +22,11 @@ const productsControllers = require("../controllers/productsControllers");
 router.get("/", productsControllers.products);
 
 /*** CREATE ONE PRODUCT ***/
-router.get("/create", productsControllers.productCreate);
-router.post("/", uploader.single("product-image"), productsControllers.store);
-
-/*** GET ONE PRODUCT ***/
-router.get("/:id", productsControllers.productDetail);
-
-/*** BUY PRODUCT ***/
-router.get("/productCart", productsControllers.productCart);
-
-/*** EDIT ONE PRODUCT***/
-router.get("/:id/edit", productsControllers.edit);
-router.put("/:id", uploader.single("image"), productsControllers.update);
-
-/*** DELETE ONE PRODUCT***/
-router.delete("/:id", productsControllers.destroy);
-
-module.exports = router;
-=======
-//Require
-
-const express = require("express");
-const router = express.Router();
-const multer = require("multer");
-const path = require("path");
-
-const storage = multer.diskStorage({
-  destination: path.join(__dirname, "../public/images/product-images"),
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-
-const uploader = multer({
-  storage,
-});
-
-const productsControllers = require("../controllers/productsControllers");
-
-/*** GET ALL PRODUCTS ***/
-router.get("/", productsControllers.products);
-
-/*** CREATE ONE PRODUCT ***/
-router.get("/create", productsControllers.productCreate);
+router.get(
+    "/create",
+    //    uploader.single("image"),
+    productsControllers.productCreate
+);
 router.post("/", uploader.single("image"), productsControllers.store);
 
 /*** GET ONE PRODUCT ***/
@@ -75,11 +36,10 @@ router.get("/:id", productsControllers.productDetail);
 router.get("/productCart", productsControllers.productCart);
 
 /*** EDIT ONE PRODUCT***/
-router.get("/:id/edit", productsControllers.edit);
+router.get("/:id/edit", uploader.single("image"), productsControllers.edit);
 router.put("/:id", uploader.single("image"), productsControllers.update);
 
 /*** DELETE ONE PRODUCT***/
 router.delete("/:id", productsControllers.destroy);
 
 module.exports = router;
->>>>>>> 7d49cf4a30e85bb93b6b712574f05b4ba5f9d00a:routes/productsRoutes.js
