f4db02104fd3228040bd3cf39531979929c9aab8
Bárbara Rivas <barbararivas@MacBook-Air-de-Barbara.local>
2021-11-05 15:08:17 +0100

    eliminé carpetas duplicadas

---
+++ database/models/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require("Sequelize");
+
+module.exports = (sequelize) => {
+  const model = sequelize.define(
+    "User",
+    {
+      id: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      price: DataTypes.DECIMAL,
+      description: DataTypes.STRING,
+      image: DataTypes.STRING,
+      delete: DataTypes.INTEGER,
+      category: DataTypes.STRING,
+    },
+    {
+      tableName: "products",
+      timestamps: false,
+    }
+  );
+
+  //Asociaciones
+  model.associate = (models) => {
+    model.belongsToMany(models.Cart, {
+      as: "cart",
+      through: "compra_producto",
+      foreignKey: "id_product",
+      otherKey: "id_cart",
+      timestamps: false,
+    });
+  };
+
+  return model;
+};
