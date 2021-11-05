8a544e255584b6239c1b3ea987d782515749b75b
juansv2k2 <juansv2k2@hotmail.com>
2021-11-05 14:40:49 +0100

    cart

---
+++ database/models/Cart.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("Sequelize");
+
+module.exports = (sequelize) => {
+    const model = sequelize.define(
+        "Cart",
+        {
+            total_price: DataTypes.DECIMAL,
+            date: DataTypes.DATE,
+            user_id: DataTypes.INTEGER,
+        },
+        {
+            tableName: "",
+            timestamps: false,
+        }
+    );
+
+    //Asociaciones
+    model.associate = (models) => {
+        model.belongsTo(models.User, { as: "user", foreignKey: "id_user" });
+        model.belongsToMany(models.Product, {
+            as: "products",
+            through: "compra_producto",
+            foreignKey: "ID_cart",
+            otherKey: "ID_product_id",
+            timestamps: false,
+        });
+    };
+
+    return model;
+};
